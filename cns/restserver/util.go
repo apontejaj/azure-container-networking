67abcd4ef8e3b4506db1b3fd2dd9caea320f3d0a
paulyufan2 <paulyu01@outlook.com>
2023-11-27 14:43:37 -0500

    s360 issue fix

--- cns/restserver/util.go
+++ cns/restserver/util.go
@@ -798,9 +798,6 @@ func isPrivateIP(ip net.IP) bool {
 		"172.16.0.0/12",  // RFC1918
 		"192.168.0.0/16", // RFC1918
 		"169.254.0.0/16", // RFC3927 link-local
-		"::1/128",        // IPv6 loopback
-		"fe80::/10",      // IPv6 link-local
-		"fc00::/7",       // IPv6 unique local addr
 	} {
 		_, block, err := net.ParseCIDR(cidr)
 		if err != nil {
