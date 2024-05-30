f8116226e9024031228462091d7f0182ca62ba7b
Kevin <t-kcamacho@microsoft.com>
2024-05-30 20:21:29 +0000

    First Iteration of SetOrchestratorInfo function

--- cns/grpc/server.go
+++ cns/grpc/server.go
@@ -1,13 +1,20 @@
 package grpc
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
 	"strconv"
+	"sync"
+	"time"
 
+	"github.com/Azure/azure-container-networking/cns"
 	pb "github.com/Azure/azure-container-networking/cns/grpc/cnsv1alpha"
+	"github.com/Azure/azure-container-networking/cns/logger"
 	"github.com/Azure/azure-container-networking/cns/restserver"
+	"github.com/Azure/azure-container-networking/cns/types"
+	"github.com/Azure/azure-container-networking/store"
 	"github.com/pkg/errors"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
@@ -30,8 +37,11 @@ type ServerSettings struct {
 // CNSService defines the CNS gRPC service.
 type CNS struct {
 	pb.UnimplementedCNSServer
+	sync.RWMutex
 	Logger *zap.Logger
-	State  *restserver.HTTPRestService
+	*restserver.HTTPRestService
+	state *restserver.HttpRestServiceState
+	store store.KeyValueStore
 }
 
 // NewServer initializes a new gRPC server instance.
@@ -71,4 +81,66 @@ func (s *Server) Start() error {
 	}
 
 	return nil
+}
+
+// areNCsPresent returns true if NCs are present in CNS, false if no NCs are present.
+func (s *CNS) areNCsPresent() bool {
+	if len(s.state.ContainerStatus) == 0 && len(s.state.ContainerIDByOrchestratorContext) == 0 {
+		return false
+	}
+	return true
+}
+
+// SaveState persists the current state of the service.
+func (s *CNS) SaveState() error {
+	// Skip if a store is not provided.
+	if s.store == nil {
+		s.Logger.Warn("Store not initialized.")
+		return nil
+	}
+
+	// Update time stamp.
+	s.state.TimeStamp = time.Now()
+	err := s.store.Write("ContainerNetworkService", s.state)
+	if err != nil {
+		s.Logger.Error("Failed to save state", zap.Error(err))
+	}
+
+	return err
+}
+
+// SetOrchestratorInfo handles setting the orchestrator information for a node.
+func (s *CNS) SetOrchestratorInfo(ctx context.Context, req *pb.SetOrchestratorInfoRequest) (*pb.SetOrchestratorInfoResponse, error) {
+	s.Logger.Info("SetOrchestratorInfo called", zap.String("nodeID", req.NodeID), zap.String("orchestratorType", req.OrchestratorType))
+
+	s.Lock()
+
+	nodeID := s.state.NodeID
+
+	var returnMessage string
+	var returnCode types.ResponseCode
+
+	if nodeID == "" || nodeID == req.NodeID || !s.areNCsPresent(){
+		switch req.OrchestratorType {
+		case cns.ServiceFabric, cns.Kubernetes, cns.KubernetesCRD, cns.WebApps, cns.Batch, cns.DBforPostgreSQL, cns.AzureFirstParty:
+			s.state.OrchestratorType = req.OrchestratorType
+			s.state.NodeID = req.NodeID
+			logger.SetContextDetails(req.OrchestratorType, req.NodeID)
+			s.SaveState()
+			returnMessage = "Orchestrator information set successfully"
+			returnCode = types.Success
+		default:
+			returnMessage = fmt.Sprintf("Invalid Orchestrator type %v", req.OrchestratorType)
+			returnCode = types.UnsupportedOrchestratorType
+		}
+	} else {
+		returnMessage = fmt.Sprintf("Invalid request since this node has already been registered as %s", nodeID)
+		returnCode = types.InvalidRequest
+	}
+
+	s.Logger.Info("SetOrchestratorInfo response", zap.String("returnMessage", returnMessage), zap.Int("returnCode", int(returnCode)))
+	resp := &pb.SetOrchestratorInfoResponse{}
+
+	s.Unlock()
+	return resp, nil
 }
