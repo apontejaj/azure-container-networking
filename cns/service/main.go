3f1b8b2eda8249957672eeba5e4d956de507d0fc
shreyashastantram <shreyash@microsoft.com>
2024-06-17 17:05:42 -0400

    Moving function out of restserver

--- cns/service/main.go
+++ cns/service/main.go
@@ -824,7 +824,7 @@ func main() {
 		if cnsconfig.EnableSwiftV2 && cnsconfig.SWIFTV2Mode == configuration.K8sSWIFTV2 {
 			// No-op for linux, mapping is set for windows in aks swiftv2 scenario
 			logger.Printf("Fetcing backend nics for debug")
-			if err = httpRemoteRestService.SetPnpIDMacaddressMapping(context.Background()); err != nil {
+			if httpRemoteRestService.PnpIDByMacAddress, err = restserver.GetPnpIDMacaddressMapping(rootCtx); err != nil {
 				logger.Errorf("Failed to fetch PnpIDMacaddress mapping")
 			}
 		}
