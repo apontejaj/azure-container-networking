ea2d744022c4ccf7db94d70e8c84d83690594f90
paulyufan2 <paulyu01@outlook.com>
2024-07-02 16:07:30 -0400

    enhance logic for accelnet nic netowrk flag

--- network/network_windows.go
+++ network/network_windows.go
@@ -303,8 +303,7 @@ func (nm *networkManager) configureHcnNetwork(nwInfo *EndpointInfo, extIf *exter
 	}
 
 	if nwInfo.NICType == cns.NodeNetworkInterfaceAccelnetFrontendNIC {
-		hcnNetwork.Type = hcn.Transparent
-		hcnNetwork.Flags = 9216 // temp hardcode until hcsshim supports the flag
+		hcnNetwork.Flags = hcn.DisableHostPort || 8192 // temp hardcode until hcsshim supports the flag
 	}
 
 	// Populate subnets.
