a461f7a9507fb249879e69f2445caa376786dc1f
aggarwal0009 <127549148+aggarwal0009@users.noreply.github.com>
2024-07-11 00:59:47 +0000

    add MacAddress o NI CRD status

--- crd/multitenancy/api/v1alpha1/nodeinfo.go
+++ crd/multitenancy/api/v1alpha1/nodeinfo.go
@@ -15,7 +15,6 @@ import (
 // +kubebuilder:subresource:status
 // +kubebuilder:resource:shortName=ni,scope=Cluster,path=nodeinfo
 // +kubebuilder:printcolumn:name="VMUniqueID",type=string,priority=0,JSONPath=`.spec.vmUniqueID`
-// +kubebuilder:printcolumn:name="Status",type=string,priority=1,JSONPath=`.status.status`
 type NodeInfo struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
@@ -46,8 +45,8 @@ type NodeInfoStatus struct {
 }
 
 type DeviceInfo struct {
-	DeviceType  DeviceType `json:"deviceType,omitempty"`
-	DeviceCount int        `json:"deviceCount"`
+	DeviceType     DeviceType `json:"deviceType,omitempty"`
+	MacAddressList []string   `json:"macAddressList"`
 }
 
 func init() {
