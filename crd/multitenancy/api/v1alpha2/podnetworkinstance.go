b93a5e577568d221ddd4c495c1adee29a32ec783
Isaac <jojosep@microsoft.com>
2024-07-26 23:08:15 +0000

    add cel validation rules
    Signed-off-by: GitHub <noreply@github.com>

--- crd/multitenancy/api/v1alpha2/podnetworkinstance.go
+++ crd/multitenancy/api/v1alpha2/podnetworkinstance.go
@@ -18,6 +18,9 @@ import (
 // +kubebuilder:metadata:labels=managed=
 // +kubebuilder:metadata:labels=owner=
 // +kubebuilder:printcolumn:name="Status",type=string,JSONPath=`.status.status`
+// +kubebuilder:validation:rule=`size(filter(self.clusterNetworkConfig.routes, x, toLower(x) == "default")) +
+// size(filter(flatten(map(self.podNetworkConfigs, p, p.routes)), x, toLower(x) == "default")) <= 1`
+// +kubebuilder:validation:message="Only one default route is allowed across cluster and pod network configurations."
 type PodNetworkInstance struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
@@ -36,6 +39,8 @@ type PodNetworkInstanceList struct {
 }
 
 // PodNetworkConfig describes a template for how to attach a PodNetwork to a Pod
+// +kubebuilder:validation:rule='self.policyBasedRouting || self.routes.size() > 0'
+// +kubebuilder:validation:message="Routes list shouldn't be empty if policybasedRouting is disabled."
 type PodNetworkConfig struct {
 	// PodNetwork is the name of a PodNetwork resource
 	PodNetwork string `json:"podNetwork"`
@@ -43,13 +48,25 @@ type PodNetworkConfig struct {
 	// +kubebuilder:default=0
 	PodIPReservationSize int `json:"podIPReservationSize,omitempty"`
 	// Routes is a list of routes to add to the Pod through interface assigned to this PodNetwork
+	// +kubebuilder:validation:rule=`!(self.routes.all(x, toLower(x) == "delegatedsubnet"))`
+	// +kubebuilder:validation:message="DelegatedSubnet is not allowed in cluster network configuration routes."
 	Routes []string `json:"routes,omitempty"`
+	// PolicyBasedRouting is a flag to enable policy based routing
+	// +kubebuilder:default=true
+	PolicyBasedRouting bool `json:"policyBasedRouting,omitempty"`
 }
 
 // ClusterNetworkConfig describes a template for how to attach the infra network to a Pos
+// +kubebuilder:validation:rule='self.policyBasedRouting || self.routes.size() > 0'
+// +kubebuilder:validation:message="Routes list shouldn't be empty if policybasedRouting is disabled."
 type ClusterNetworkConfig struct {
 	// Routes is a list of routes to add to the Pod through interface assigned to the infra network
+	// +kubebuilder:validation:rule=`!(self.routes.all(x, toLower(x) == "delegatedsubnet"))`
+	// +kubebuilder:validation:message="DelegatedSubnet is not allowed in cluster network configuration routes."
 	Routes []string `json:"routes,omitempty"`
+	// PolicyBasedRouting is a flag to enable policy based routing
+	// +kubebuilder:default=true
+	PolicyBasedRouting bool `json:"policyBasedRouting,omitempty"`
 }
 
 // PodNetworkInstanceSpec defines the desired state of PodNetworkInstance
@@ -65,9 +82,15 @@ type PodNetworkInstanceSpec struct {
 // PodNetworkInstanceStatus defines the observed state of PodNetworkInstance
 type PodNetworkInstanceStatus struct {
 	// +kubebuilder:validation:Optional
-	PodIPAddresses     []string             `json:"podIPAddresses,omitempty"`
-	Status             PNIStatus            `json:"status,omitempty"`
-	PodNetworkStatuses map[string]PNIStatus `json:"podNetworkStatuses,omitempty"`
+	Status                   PNIStatus                `json:"status,omitempty"`
+	PodNetworkConfigStatuses []PodNetworkConfigStatus `json:"PodNetworkConfigStatuses,omitempty"`
+}
+
+// PodNetworkStatus describes the status of a PodNetwork
+type PodNetworkConfigStatus struct {
+	// +kubebuilder:validation:Optional
+	Status         PNIStatus `json:"status,omitempty"`
+	PodIPAddresses []string  `json:"podIPAddresses,omitempty"`
 }
 
 // PNIStatus indicates the status of PNI
