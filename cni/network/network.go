6b92514bdbacae768cef94b9aba7e4411a9020fa
QxBytes <39818795+QxBytes@users.noreply.github.com>
2024-07-31 15:27:04 -0700

    backport: test: validate inputs to acn components (#2845) (#2889)
    test: validate inputs to acn components (#2845)
    
    * add validation for cni args and cns request values
    
    * address feedback
    
    * simplify regex match

--- cni/network/network.go
+++ cni/network/network.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"regexp"
 	"strconv"
 	"time"
 
@@ -34,6 +35,9 @@ import (
 	"github.com/pkg/errors"
 	"go.uber.org/zap"
 )
+
+// matches if the string fully consists of zero or more alphanumeric, dots, dashes, parentheses, spaces, or underscores
+var allowedInput = regexp.MustCompile(`^[a-zA-Z0-9._\-\(\) ]*$`)
 
 const (
 	dockerNetworkOption = "com.docker.network.generic"
@@ -344,6 +348,11 @@ func (plugin *NetPlugin) Add(args *cniSkel.CmdArgs) error {
 	nwCfg, err := cni.ParseNetworkConfig(args.StdinData)
 	if err != nil {
 		err = plugin.Errorf("Failed to parse network configuration: %v.", err)
+		return err
+	}
+
+	if argErr := plugin.validateArgs(args, nwCfg); argErr != nil {
+		err = argErr
 		return err
 	}
 
@@ -871,6 +880,11 @@ func (plugin *NetPlugin) Get(args *cniSkel.CmdArgs) error {
 
 	logger.Info("Read network configuration", zap.Any("config", nwCfg))
 
+	if argErr := plugin.validateArgs(args, nwCfg); argErr != nil {
+		err = argErr
+		return err
+	}
+
 	iptables.DisableIPTableLock = nwCfg.DisableIPTableLock
 
 	// Initialize values from network config.
@@ -951,6 +965,11 @@ func (plugin *NetPlugin) Delete(args *cniSkel.CmdArgs) error {
 	// Parse network configuration from stdin.
 	if nwCfg, err = cni.ParseNetworkConfig(args.StdinData); err != nil {
 		err = plugin.Errorf("[cni-net] Failed to parse network configuration: %v", err)
+		return err
+	}
+
+	if argErr := plugin.validateArgs(args, nwCfg); argErr != nil {
+		err = argErr
 		return err
 	}
 
@@ -1132,6 +1151,11 @@ func (plugin *NetPlugin) Update(args *cniSkel.CmdArgs) error {
 	// Parse network configuration from stdin.
 	if nwCfg, err = cni.ParseNetworkConfig(args.StdinData); err != nil {
 		err = plugin.Errorf("Failed to parse network configuration: %v.", err)
+		return err
+	}
+
+	if argErr := plugin.validateArgs(args, nwCfg); argErr != nil {
+		err = argErr
 		return err
 	}
 
@@ -1395,4 +1419,15 @@ func convertCniResultToInterfaceInfo(result *cniTypesCurr.Result) network.Interf
 	}
 
 	return interfaceInfo
+}
+
+func (plugin *NetPlugin) validateArgs(args *cniSkel.CmdArgs, nwCfg *cni.NetworkConfig) error {
+	if !allowedInput.MatchString(args.ContainerID) || !allowedInput.MatchString(args.IfName) {
+		return errors.New("invalid args value")
+	}
+	if !allowedInput.MatchString(nwCfg.Bridge) {
+		return errors.New("invalid network config value")
+	}
+
+	return nil
 }
