5629b366a5b4464d604784f80b065f466f9f809c
Isaac <jojosep@microsoft.com>
2024-08-06 17:24:55 +0000

    v1beta1 without webhook
    Signed-off-by: GitHub <noreply@github.com>

--- crd/multitenancy/api/v1beta1/podnetworkinstance.go
+++ crd/multitenancy/api/v1beta1/podnetworkinstance.go
@@ -1,7 +1,7 @@
 //go:build !ignore_uncovered
 // +build !ignore_uncovered
 
-package v1alpha1
+package v1beta1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -13,12 +13,11 @@ import (
 
 // PodNetworkInstance is the Schema for the PodNetworkInstances API
 // +kubebuilder:resource:shortName=pni,scope=Namespaced
-// +kubebuilder:storageversion
 // +kubebuilder:subresource:status
 // +kubebuilder:metadata:labels=managed=
 // +kubebuilder:metadata:labels=owner=
 // +kubebuilder:printcolumn:name="Status",type=string,JSONPath=`.status.status`
-// +kubebuilder:printcolumn:name="PodNetworks",priority=1,type=string,JSONPath=`.spec.podNetworks`
+// +kubebuilder:unservedversion
 type PodNetworkInstance struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
@@ -37,34 +36,55 @@ type PodNetworkInstanceList struct {
 }
 
 // PodNetworkConfig describes a template for how to attach a PodNetwork to a Pod
+// +kubebuilder:validation:XValidation:rule="self.policyBasedRouting || self.routes.size() > 0",message="Routes list shouldn't be empty if policybasedRouting is disabled."
 type PodNetworkConfig struct {
 	// PodNetwork is the name of a PodNetwork resource
 	PodNetwork string `json:"podNetwork"`
 	// PodIPReservationSize is the number of IP address to statically reserve
 	// +kubebuilder:default=0
 	PodIPReservationSize int `json:"podIPReservationSize,omitempty"`
+	// Routes is a list of routes to add to the Pod through interface assigned to this PodNetwork
+	// +kubebuilder:default=[]
+	Routes []string `json:"routes,omitempty"`
+	// PolicyBasedRouting is a flag to enable policy based routing
+	// +kubebuilder:default=true
+	PolicyBasedRouting bool `json:"policyBasedRouting,omitempty"`
+}
+
+// ClusterNetworkConfig describes a template for how to attach the infra network to a Pos
+// +kubebuilder:validation:XValidation:rule="self.policyBasedRouting || self.routes.size() > 0",message="Routes list shouldn't be empty if policybasedRouting is disabled."
+type ClusterNetworkConfig struct {
+	// +kubebuilder:default=[]
+	// Routes is a list of routes to add to the Pod through interface assigned to the infra network
+	Routes []string `json:"routes,omitempty"`
+	// PolicyBasedRouting is a flag to enable policy based routing
+	// +kubebuilder:default=true
+	PolicyBasedRouting bool `json:"policyBasedRouting,omitempty"`
 }
 
 // PodNetworkInstanceSpec defines the desired state of PodNetworkInstance
 type PodNetworkInstanceSpec struct {
-	// Deprecated - use PodNetworks
-	// +kubebuilder:validation:Optional
-	PodNetwork string `json:"podnetwork,omitempty"`
-	// Deprecated - use PodNetworks
-	// +kubebuilder:default=0
-	PodIPReservationSize int `json:"podIPReservationSize,omitempty"`
+	// ClusterNetworkConfig describes how to attach the infra network to a Pod
+	ClusterNetworkConfig ClusterNetworkConfig `json:"clusterNetworkConfig"`
 	// PodNetworkConfigs describes each PodNetwork to attach to a single Pod
-	// optional for now in case orchestrator uses the deprecated fields
-	// +kubebuilder:validation:Optional
 	PodNetworkConfigs []PodNetworkConfig `json:"podNetworkConfigs"`
+}
+
+// PodNetworkConfigStatus is the status of the PodNetworkConfig
+type PodNetworkConfigStatus struct {
+	// +kubebuilder:validation:Optional
+	Status PNIStatus `json:"status,omitempty"`
+	// +kubebuilder:validation:Optional
+	PodIPAddresses []string `json:"podIPAddresses,omitempty"`
 }
 
 // PodNetworkInstanceStatus defines the observed state of PodNetworkInstance
 type PodNetworkInstanceStatus struct {
+	// Status indicates the status of PNI
+	Status PNIStatus `json:"status,omitempty"`
+	// PodNetworkConfigStatuses describes the status of each PodNetworkConfig
 	// +kubebuilder:validation:Optional
-	PodIPAddresses     []string             `json:"podIPAddresses,omitempty"`
-	Status             PNIStatus            `json:"status,omitempty"`
-	PodNetworkStatuses map[string]PNIStatus `json:"podNetworkStatuses,omitempty"`
+	PodNetworkConfigStatuses []PodNetworkConfigStatus `json:"podNetworkConfigStatuses,omitempty"`
 }
 
 // PNIStatus indicates the status of PNI
