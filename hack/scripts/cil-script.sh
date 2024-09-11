c76cdeac1c2d667e3fd23e4438f7ebfe145987b2
jpayne3506 <payne.3506@gmail.com>
2024-09-11 11:50:56 -0700

    ci: improve script and test resource names

--- hack/scripts/cil-script.sh
+++ hack/scripts/cil-script.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 # Requires
-# prefix1 - unique single digit whole number 1-9. Cannot match prefix2
-# prefix2 - unique single digit whole number 1-9. Cannot match prefix1
+# sufix1 - unique single digit whole number 1-9. Cannot match sufix2
+# sufix2 - unique single digit whole number 1-9. Cannot match sufix1
 # SUB - GUID for subscription
 # clusterType - overlay-byocni-nokubeproxy-up-mesh is primary atm, but leaving for testing later.
-# Example command: prefix1=1 prefix2=2 SUB=<GUID> clusterType=overlay-byocni-nokubeproxy-up-mesh ./cli-script.sh
+# Example command: clusterPrefix=<alais> sufix1=1 sufix2=2 SUB=<GUID> clusterType=overlay-byocni-nokubeproxy-up-mesh ./cil-script.sh
 
-prefixes="${prefix1} ${prefix2}"
+sufixes="${sufix1} ${sufix2}"
 install=helm
-echo "Prefixes ${prefixes}"
+echo "sufixes ${sufixes}"
 
 cd ../..
-for unique in $prefixes; do
+for unique in $sufixes; do
     make -C ./hack/aks $clusterType \
         AZCLI=az REGION=westus2 SUB=$SUB \
-        CLUSTER=cilpoc-${unique} \
+        CLUSTER=${clusterPrefix}-${unique} \
         POD_CIDR=192.${unique}0.0.0/16 SVC_CIDR=192.${unique}1.0.0/16 DNS_IP=192.${unique}1.0.10 \
-        VNET_PREFIX=10.${unique}0.0.0/16 SUBNET_PREFIX=10.${unique}0.0.0/16
+        VNET_sufix=10.${unique}0.0.0/16 SUBNET_sufix=10.${unique}0.0.0/16
 
     if [ $install == "helm" ]; then
         cilium install -n kube-system cilium cilium/cilium --version v1.16.1 \
-        --set azure.resourceGroup=cilpoc-${unique} --set cluster.id=${unique} \
+        --set azure.resourceGroup=${clusterPrefix}-${unique}-rg --set cluster.id=${unique} \
         --set ipam.operator.clusterPoolIPv4PodCIDRList='{192.'${unique}'0.0.0/16}' \
         --set hubble.enabled=false \
         --set envoy.enabled=false
@@ -41,35 +41,31 @@ done
 cd hack/scripts
 
 VNET_ID1=$(az network vnet show \
-    --resource-group "cilpoc-${prefix1}" \
-    --name "cilpoc-${prefix1}" \
+    --resource-group "${clusterPrefix}-${sufix1}-rg" \
+    --name "${clusterPrefix}-${sufix1}-vnet" \
     --query id -o tsv)
 
 VNET_ID2=$(az network vnet show \
-    --resource-group "cilpoc-${prefix2}" \
-    --name "cilpoc-${prefix2}" \
+    --resource-group "${clusterPrefix}-${sufix2}-rg" \
+    --name "${clusterPrefix}-${sufix2}-vnet" \
     --query id -o tsv)
 
 az network vnet peering create \
-    -g "cilpoc-${prefix1}" \
-    --name "peering-cilpoc-${prefix1}-to-cilpoc-${prefix2}" \
-    --vnet-name "cilpoc-${prefix1}" \
+    -g "${clusterPrefix}-${sufix1}-rg" \
+    --name "peering-${clusterPrefix}-${sufix1}-to-${clusterPrefix}-${sufix2}" \
+    --vnet-name "${clusterPrefix}-${sufix1}-vnet" \
     --remote-vnet "${VNET_ID2}" \
     --allow-vnet-access
 
 az network vnet peering create \
-    -g "cilpoc-${prefix2}" \
-    --name "peering-cilpoc-${prefix2}-to-cilpoc-${prefix1}" \
-    --vnet-name "cilpoc-${prefix2}" \
+    -g "${clusterPrefix}-${sufix2}-rg" \
+    --name "peering-${clusterPrefix}-${sufix2}-to-${clusterPrefix}-${sufix1}" \
+    --vnet-name "${clusterPrefix}-${sufix2}-vnet" \
     --remote-vnet "${VNET_ID1}" \
     --allow-vnet-access
 
-az aks get-credentials \
-    --resource-group "cilpoc-${prefix2}" \
-    --name "cilpoc-${prefix2}"
-
 # Retaining for testing
-# cilium install -n kube-system cilium cilium/cilium --version v1.16.1 --set azure.resourceGroup=cilpoc-${unique} \
+# cilium install -n kube-system cilium cilium/cilium --version v1.16.1 --set azure.resourceGroup=${clusterPrefix}-${unique} \
 # --set cluster.id=${unique} --set ipam.operator.clusterPoolIPv4PodCIDRList='{10.'${unique}'0.0.0/16}' \
 # --set ipam.mode="delegated-plugin" \
 # --set hubble.enabled=false \
@@ -81,32 +77,32 @@ az aks get-credentials \
 # --set kube-proxy-replacement-healthz-bind-address="0.0.0.0:10256"
 
 
-cilium clustermesh enable --context cilpoc-${prefix1} --enable-kvstoremesh=false
-cilium clustermesh enable --context cilpoc-${prefix2} --enable-kvstoremesh=false
+cilium clustermesh enable --context ${clusterPrefix}-${sufix1} --enable-kvstoremesh=false
+cilium clustermesh enable --context ${clusterPrefix}-${sufix2} --enable-kvstoremesh=false
 # -- testing --
-# cilium clustermesh enable --context cilpoc-${prefix1} --enable-kvstoremesh=true
-# cilium clustermesh enable --context cilpoc-${prefix2} --enable-kvstoremesh=true
+# cilium clustermesh enable --context ${clusterPrefix}-${sufix1} --enable-kvstoremesh=true
+# cilium clustermesh enable --context ${clusterPrefix}-${sufix2} --enable-kvstoremesh=true
 # -- testing --
 
-cilium clustermesh status --context cilpoc-${prefix1} --wait
-cilium clustermesh status --context cilpoc-${prefix2} --wait
+cilium clustermesh status --context ${clusterPrefix}-${sufix1} --wait
+cilium clustermesh status --context ${clusterPrefix}-${sufix2} --wait
 
 # CA is passed between clusters in this step
-cilium clustermesh connect --context cilpoc-${prefix1} --destination-context cilpoc-${prefix2}
+cilium clustermesh connect --context ${clusterPrefix}-${sufix1} --destination-context ${clusterPrefix}-${sufix2}
 # These can be run in parallel in different bash shells
 # Running connectivity test from context to multi. test-namespace shows the direction of the test. 1->2, 2->1.
 # Completeing both of these will take 20+~minutes. Run outside of script.
-# cilium connectivity test --context cilpoc-${prefix1} --multi-cluster cilpoc-${prefix2} --test-namespace ciltest-${prefix1}-${prefix2} --force-deploy
-# cilium connectivity test --context cilpoc-${prefix2} --multi-cluster cilpoc-${prefix1} --test-namespace ciltest-${prefix2}-${prefix1} --force-deploy
+# cilium connectivity test --context ${clusterPrefix}-${sufix1} --multi-cluster ${clusterPrefix}-${sufix2} --test-namespace ciltest-${sufix1}-${sufix2} --force-deploy
+# cilium connectivity test --context ${clusterPrefix}-${sufix2} --multi-cluster ${clusterPrefix}-${sufix1} --test-namespace ciltest-${sufix2}-${sufix1} --force-deploy
 
 
 
 # -- Useful debug commands --
-# cilium status --context cilpoc-${prefix1}
-# cilium status --context cilpoc-${prefix2}
+# cilium status --context ${clusterPrefix}-${sufix1}
+# cilium status --context ${clusterPrefix}-${sufix2}
 
-# az aks get-credentials --resource-group cilpoc-${prefix1} --name cilpoc-${prefix1} --overwrite-existing
-# az aks get-credentials --resource-group cilpoc-${prefix2} --name cilpoc-${prefix2} --overwrite-existing
+# az aks get-credentials --resource-group ${clusterPrefix}-${sufix1} --name ${clusterPrefix}-${sufix1} --overwrite-existing
+# az aks get-credentials --resource-group ${clusterPrefix}-${sufix2} --name ${clusterPrefix}-${sufix2} --overwrite-existing
 
-# cilium clustermesh disable --context cilpoc-${prefix1}
-# cilium clustermesh disable --context cilpoc-${prefix2}
+# cilium clustermesh disable --context ${clusterPrefix}-${sufix1}
+# cilium clustermesh disable --context ${clusterPrefix}-${sufix2}
