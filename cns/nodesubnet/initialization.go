31bf929a5eae7a7f018c87d837963bbe7760f015
Santhosh Prabhu <sanpra1989@gmail.com>
2024-09-25 10:19:07 -0700

    new files

---
+++ cns/nodesubnet/initialization.go
@@ -0,0 +1,26 @@
+package nodesubnet
+
+import (
+	"context"
+
+	"github.com/Azure/azure-container-networking/cns"
+	"github.com/Azure/azure-container-networking/cns/ipam"
+	"github.com/pkg/errors"
+	"golang.org/x/exp/maps"
+)
+
+func reconcileInitialCNSState(ctx context.Context, ipamReconciler ipam.IpamStateReconciler, podInfoByIPProvider cns.PodInfoByIPProvider) error {
+	// Get previous PodInfo state from podInfoByIPProvider
+	podInfoByIP, err := podInfoByIPProvider.PodInfoByIP()
+	if err != nil {
+		return errors.Wrap(err, "provider failed to provide PodInfoByIP")
+	}
+
+	// Create a network container request that holds all the IPs from PodInfoByIP
+	secondaryIPs := maps.Keys(podInfoByIP)
+	ncRequest, err := CreateNodeSubnetNCRequest("", secondaryIPs)
+	if err != nil {
+		return errors.Wrap(err, "ncRequest creation failed")
+	}
+	ipamReconciler.ReconcileIPAMState([]*cns.CreateNetworkContainerRequest{ncRequest}, podInfoByIP, nil)
+}
