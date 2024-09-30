28149e15ae40c8f54c780d62c17992d0eb8cef48
Chloe Chen <chlochen@microsoft.com>
2024-09-30 13:26:12 -0700

    fix

--- .pipelines/multitenancy/scripts/run-tests.sh
+++ .pipelines/multitenancy/scripts/run-tests.sh
@@ -49,11 +49,26 @@ function run_tests() {
         return 0
     fi
     
+    # Get the OIDC Issuer URL
+    export AKS_OIDC_ISSUER="$(az aks show -n "$mt_test_cluster" -g "$RG" --query "oidcIssuerProfile.issuerUrl" -otsv)"
+
+    # Federate the identity
+    az identity federated-credential create \
+        --name "$FEDERATED_IDENTITY_CREDENTIAL_PREFIX-$mt_test_cluster" \
+        --identity-name "$USER_ASSIGNED_IDENTITY_NAME" \
+        --resource-group "$RG" \
+        --issuer "$AKS_OIDC_ISSUER" \
+        --subject system:serviceaccount:mtpod-to-service-endpoint:workload-identity-sa
+
     # Get identity client ID
     export USER_ASSIGNED_CLIENT_ID=$(az identity show --resource-group "$RG" --name "$USER_ASSIGNED_IDENTITY_NAME" --query 'clientId' -o tsv)
-
+    
+    # Run aks e2e test suite
     chmod +x ./akse2e.sh
     ./akse2e.sh $mt_test_cluster && passed="true" || passed="false"
+
+    #Clean up user-assigned identity
+    az identity federated-credential delete --name "$FEDERATED_IDENTITY_CREDENTIAL_PREFIX-$mt_test_cluster" --identity-name "$USER_ASSIGNED_IDENTITY_NAME" --resource-group "$RG" --yes
 
     if [[ "$passed" == "true" ]]; then
         echo "Tests passed"
